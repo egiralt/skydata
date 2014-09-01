363d76145e7fc56e3e81b26c89fed8cb5084df09
Ernesto Giralt <egiralt@gmail.com>
2014-09-01 17:11:02 -0500

    Nuevo metodo para los temas y corrección del método Assign.

--- Core/Controller/SkyDataController.class.php
+++ Core/Controller/SkyDataController.class.php
@@ -1,26 +1,26 @@
 <?php
 /**
  *  SkyData: CMS Framework   -  12/Aug/2014
- * 
- * Copyright (C) 2014  Ernesto Giralt (egiralt@gmail.com) 
+ *
+ * Copyright (C) 2014  Ernesto Giralt (egiralt@gmail.com)
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @Author: E. Giralt
  * @Date:   12/Aug/2014
  * @Last Modified by:   E. Giralt
  * @Last Modified time: 18/Aug/2014
  */
- 
+
 namespace SkyData\Core\Controller;
 
 use \SkyData\Core\SkyDataObject;
@@ -35,66 +35,74 @@ use \SkyData\Core\Page\IPage;
  */
 abstract class SkyDataController extends SkyDataObject implements IController, ILayoutNode
 {
-	
+
 	private $Parent;
-	private $View;	
-	
+	private $View;
+
 	/**
 	 *	Función principal que modela el comportamiento del Controller
 	 */
 	abstract public function Run ();
-	
+
 	public function SetView (IRenderable $viewInstance)
 	{
 		if ($viewInstance !== $this->View)
 			$this->View = $viewInstance;
-		
+
 		return $this->View;
 	}
-	
+
 	public function GetView()
 	{
-		return $this->View;		
+		return $this->View;
 	}
-	
+
 	public function GetParent()
 	{
-		return $this->Parent;		
+		return $this->Parent;
 	}
-	
+
 	public function SetParent (ILayoutNode $parent)
 	{
 		$this->Parent = $parent;
 	}
-	
+
 	public function Assign ($name, $object)
 	{
-		$this->GetApplication()->GetView()->Assign ($name, $object);
+		$this->GetView()->Assign ($name, $object);
 	}
-	
+
 	public function AssignError ($error)
 	{
 		$this->Assign('error', $error);
-	} 
-	
+	}
+
 	public function SetError ($description, $number = -1)
 	{
 		$result = new \stdClass ();
 		$result->error = $description;
 		$result->error_number = $number;
 		$this->AssignError($result);
-		
+
 		return $result;
 	}
-    
+
     public function GetRequestParams ()
     {
         $result = null;
         $page = $this->GetParent();
         if (isset($page) && $page instanceof IPage)
             $result = $page->GetRequestParams();
-        
-        return $result;  
+
+        return $result;
     }
     
+    /**
+     * Devuelve el tema actualmente seleccionado
+     */
+    public function GetTheme ()
+    {
+        return $this->GetApplication ()->GetView()->GetSelectedTheme();
+    }
+
 }
