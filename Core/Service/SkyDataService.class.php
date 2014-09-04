43b021e3ceea8b2d47c3140c7eab7c8ab35dc2ea
Ernesto Giralt <egiralt@gmail.com>
2014-09-04 17:07:10 -0500

    Agregado la inyeccion de variables y funciones. Corregir el path de los scripts

--- Core/Service/SkyDataService.class.php
+++ Core/Service/SkyDataService.class.php
@@ -1,20 +1,20 @@
 <?php
 /**
  *  SkyData: CMS Framework   -  12/Aug/2014
- * 
- * Copyright (C) 2014  Ernesto Giralt (egiralt@gmail.com) 
+ *
+ * Copyright (C) 2014  Ernesto Giralt (egiralt@gmail.com)
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * SkyDataService.class.php
  *
  * @Author: E. Giralt
@@ -40,10 +40,10 @@ define ('DOC_COMMENT_RENDER_TAG_PATTERN',               '/\@renderTag\s*([^\n]*)
 define ('DOC_COMMENT_RENDER_CLASS_PATTERN',             "/\@renderClass\s*([^\n]*)/");
 define ('DOC_COMMENT_RENDER_ATTRIBUTE_PATTERN',         "/\@renderAttribute\s*([^\n]*)/");
 define ('DOC_COMMENT_RENDER_TAG_PARAMETERS_PATTERN',    "/([^\W]*)\s*=\s*('[^']*'|[^\W]*)/");
- 
+
 class SkyDataService extends SkyDataResource implements IService
 {
-	
+
 	const DATATYPE_DATA 		= 'data';
 	const DATATYPE_JSON 		= 'json';
 	const DATATYPE_HTML 		= 'html';
@@ -53,23 +53,52 @@ class SkyDataService extends SkyDataResource implements IService
 	const DATATYPE_IMAGE_PNG	= 'image/png';
 	const DATATYPE_IMAGE_GIF	= 'image/gif';
 	const DATATYPE_IMAGE_TIFF	= 'image/tiff';
-	
+
 	const HTTP_VERB_GET		= 'GET';
 	const HTTP_VERB_PUT		= 'PUT';
 	const HTTP_VERB_DELETE	= 'DELETE';
 	const HTTP_VERB_POST	= 'POST';
 	const HTTP_VERB_HEAD	= 'HEAD';
-	
+
 	const METHOD_PREFIX_GET 	= 'Get';
 	const METHOD_PREFIX_DELETE 	= 'Delete';
 	const METHOD_PREFIX_POST 	= 'Post';
 	const METHOD_PREFIX_PUT 	= 'Put';
-	
+
+	private $InjectedFunctions;
+	private $InjectedVariables;
+
+	public function InjectFunction ($scriptFileName, $namedParametersArray, $loadOnStart = false)
+    {
+        // Si el nombre del fichero es una ruta relativa, se le agrega la ruta actual
+        if (substr($scriptFileName,0,1) !== '/')
+            $scriptFileName = realpath(ReflectionFactory::GetClassDirectory (get_class($this)).'/'.$scriptFileName);
+            
+        if (is_file($scriptFileName))
+        {
+            $scriptInfo = new \stdClass();
+            $scriptInfo->parameters = $namedParametersArray;
+            $scriptInfo->script = $scriptFileName;
+            $scriptInfo->loadOnStart = $loadOnStart;            
+
+            $this->InjectedFunctions[$scriptFileName] = $scriptInfo;
+        }
+    }
+
+    public function InjectVariable ($variableName, $initValue)
+    {
+        $variableInfo = new \stdClass();
+        $variableInfo->variable = $variableName;
+        $variableInfo->value = $initValue;
+
+        $this->InjectedVariables[] = $variableInfo;
+    }
+
 	public function GetLastModificationTime()
     {
-        return null;    
+        return null;
     }
-    
+
 	/**
 	 * Retorna una instancia de clase por defecto a crear cuando no se encuentre un View para el actual módulo
 	 */
@@ -77,7 +106,8 @@ class SkyDataService extends SkyDataResource implements IService
 	{
 		return new \SkyData\Core\View\NullView ();
 	}
-	
+
+
 	/**
 	 * Retorna una instancia de la clase por defecto a crear cuando no se encuentre un Controller para el actual módulo
 	 */
@@ -85,12 +115,12 @@ class SkyDataService extends SkyDataResource implements IService
 	{
 		return new SkyDataServiceController ();
 	}
-	
+
 	public function Exec ($methodName, $requestParams)
 	{
 		$methodExists = false; // Un flag para el chequeo de si existe el método o no
 		$result = null;
-		
+
 		$class = new \ReflectionClass ($this->GetController ());
 		$methods = $class->getMethods(\ReflectionMethod::IS_PUBLIC);
 		foreach ($methods as $method)
@@ -110,10 +140,10 @@ class SkyDataService extends SkyDataResource implements IService
 						{
 							$passParam[] = $param->getDefaultValue(); // El valor por defecto del campo (si lo tiene!)
 						}
-						catch (\Exception $e) 
+						catch (\Exception $e)
 						{
-							$passParam[] = null;	// No tiene valor por defecto, se le asigna null								
-						} 
+							$passParam[] = null;	// No tiene valor por defecto, se le asigna null
+						}
 				}
 				//echo "<pre>"; print_r ($class);die();
 				// Ya se puede llamar el método
@@ -121,37 +151,37 @@ class SkyDataService extends SkyDataResource implements IService
 				$interfaces= class_implements($result);
 				if (in_array('SkyData\Core\Model\IDataRow', $interfaces)) //TODO: Por ahora solo considerar los rows, luego las tablas
 					$result = $result->GetRawClass();
-			}			
-		}	
-		
+			}
+		}
+
 		if (!$methodExists)
 			Http::Raise404();
-			
+
 		return $result;
 	}
 
 	public function IsGlobal()
 	{
-        return true;        
-	    //$this->GetApplication()->GetConfigurationManager()->GetMapping('services') 
+        return true;
+	    //$this->GetApplication()->GetConfigurationManager()->GetMapping('services')
 		//$serviceConfig = [$this->GetClassShortName()];
-		//return isset($serviceConfig) && ($serviceConfig['global'] === true); 
+		//return isset($serviceConfig) && ($serviceConfig['global'] === true);
 	}
 
 	//TODO: Este método será para futuros desarrollos: permitir que un servicio sea declarado global para que se genere
 	// con un factory en el módulo general y que pueda ser reusable. Un servicio global, se genera desde la aplicación
-	// y puede no ser usado por páginas o módulos. Si un servicio global se usa en una página, se genera un código específico 
+	// y puede no ser usado por páginas o módulos. Si un servicio global se usa en una página, se genera un código específico
 	// para reusar ese código global y no se generará igual que se fuera local
 	public function RenderGlobalServiceJavascript()
 	{
 		$result = null;
 		if ($this->IsGlobal())
 		{
-		}	
-		
-		return $result;	
+		}
+
+		return $result;
 	}
-	
+
 	/**
 	 * Genera el código javascript de este servicio, compatible con Angular
 	 */
@@ -160,7 +190,7 @@ class SkyDataService extends SkyDataResource implements IService
 		// Se construye un objeto que contiene cada uno de las tablas y los métodos que contiene
 		$ajaxMethods = $this->GetAjaxMethods();
 		$dataModel = array();
-		foreach ($this->GetDataModel() as $modelName => $fields) 
+		foreach ($this->GetDataModel() as $modelName => $fields)
 		{
 			$modelInfo = new \stdClass();
 			$modelInfo->name = $modelName;
@@ -175,7 +205,7 @@ class SkyDataService extends SkyDataResource implements IService
 			$modelInfo->is_enumerable = preg_match('/.*s$/i', $modelName);
 			$modelInfo->methods = array_filter($modelInfo->methods, 'count'); // Eliminar los valores null
 			// Hay que buscar los métodos que no usan los verbs por defecto, pero que están conectados al modelo de datos también
-			foreach ($ajaxMethods as $methodName => $methodInfo) 
+			foreach ($ajaxMethods as $methodName => $methodInfo)
 			{
 				if ($methodInfo->bind_model == $modelName)
 					$modelInfo->methods[] = $methodInfo;
@@ -184,38 +214,53 @@ class SkyDataService extends SkyDataResource implements IService
 		}
 		// Ahora hay que hacer una lista con el resto de métodos, eliminando los que están asociados al modelo de datos
 		$dataMethods = array();
-		foreach ($dataModel as $model) 
+		foreach ($dataModel as $model)
 		{
-			foreach ($model->methods as $methodInfo) 
+			foreach ($model->methods as $methodInfo)
 				$dataMethods[] = $methodInfo->name;
 		}
 		// generar la lista del resto de métodos
 		$otherMethods = array();
-		foreach ($ajaxMethods as $methodName => $methodInfo) 
+		foreach ($ajaxMethods as $methodName => $methodInfo)
 		{
 			if (!in_array($methodName, $dataMethods))
 				$otherMethods[] = $methodInfo;
 		}
+
+        // Leer el contenido de los scripts inyectables 
+        $InjectedFunctions = array();
+        foreach ($this->InjectedFunctions as $fileName => $info)
+        {
+            
+            $newInfo = clone $info;
+            $newInfo->script = file_get_contents(realpath($fileName));
+            $newInfo->name = basename($fileName, '.js');
+            $InjectedFunctions[] = $newInfo;
+        }
+        
 		// Preparar los parámetros para la plantilla
 		//echo "<pre>";print_r ($dataModel); die();
 		$params = array (
-			'serviceName' 	=> $this->GetClassShortName(),
-			'tables' 		=> $dataModel,
-			'methods' 		=> $otherMethods,
-			'is_global'		=> $this->IsGlobal()
-		);	
-		
+		    'base_path'          => $this->GetApplication()->GetApplicationBaseUrl(),
+			'serviceName'        => $this->GetClassShortName(),
+			'tables'             => $dataModel,
+			'methods' 		     => $otherMethods,
+			'is_global'          => $this->IsGlobal(),
+			'InjectedFunctions'    => $InjectedFunctions,
+            'injectedVariables'  => $this->InjectedVariables
+		);
+
 		// Se utilizarán las plantillas predeterminadas guardas, y generadas con Twig
 		$result = TwigHelper::RenderTemplate (SKYDATA_PATH_CORE.'/Service/Templates/service_controller_factory.twig', $params);
-		
-		return $result;		
+
+		return $result;
 	}
-	
+
 	public function GetDataModel ()
 	{
 		return $this->GetConfigurationManager()->GetMapping ('datamodel');
 	}
-	
+
 	/**
 	 * Retorna una lista de los métodos que se encargan de atender las solicitudes ajax con la información asociada
 	 * de las marcas
@@ -223,82 +268,87 @@ class SkyDataService extends SkyDataResource implements IService
 	public function GetAjaxMethods()
 	{
 		$result = array();
-		$class = new \ReflectionClass ($this->GetController ());
-		$methods = $class->getMethods(\ReflectionMethod::IS_PUBLIC);
-		foreach ($methods as $method)
-		{
-			$methodDocumentation = $method->getDocComment();
-			// Se procesa si el método tiene el decorator
-			if (preg_match(DOC_COMMENT_AJAX_METHOD_PATTERN, $methodDocumentation))
-			{
-				$methodInfo = new \stdClass();
-				$methodInfo->name = $method->getName();
-				$methodInfo->is_global = $this->IsGlobal(); // Para garantizar que el servicio se genere o local o usando el global.
-				$methodInfo->run_on_load = preg_match(DOC_COMMENT_RUN_ON_LOAD_PATTERN, $methodDocumentation);
-				// Tiene variables asociadas?
-				if (preg_match(DOC_COMMENT_BIND_VARIABLE_PATTERN, $methodDocumentation, $matches))
-					$methodInfo->bind_variable =$matches[1]; // Se toma el nombre de la variable que genera este valor
-				//Está conectado al modelo de datos?
-				if (preg_match(DOC_COMMENT_BIND_DATA_MODEL_PATTERN, $methodDocumentation, $matches))
-					$methodInfo->bind_model =$matches[1]; // Se toma el nombre de la variable que genera este valor
-
-				//Qué tipo de salida tiene?
-				if (preg_match(DOC_COMMENT_CONTENT_TYPE_PATTERN, $methodDocumentation, $matches))
-					$methodInfo->contentType =$matches[1]; // Se toma el nombre de la variable que genera este valor
-
-				// Generará un tag?
-				if (preg_match(DOC_COMMENT_RENDER_TAG_PATTERN, $methodDocumentation, $matches))
-				{
-					$methodInfo->render_tag = true;
-					$methodInfo->type_of_render = 'E';
-					$methodInfo->tag = new \stdClass();
-					$methodInfo->tag->render_as = array();
-					$this->BuildRenderDecorator($matches[1], $methodInfo);
-					//echo "<pre>";print_r ($methodInfo); 
-				} 
-				// Generará una clase?
-				if (preg_match(DOC_COMMENT_RENDER_CLASS_PATTERN, $methodDocumentation, $matches))
-				{
-					$methodInfo->render_class = true;
-					$methodInfo->type_of_render = 'C';
-					$methodInfo->tag = new \stdClass();
-					$methodInfo->tag->render_as = array();
-					$this->BuildRenderDecorator($matches[1], $methodInfo);
-					//echo "<pre>";print_r ($methodInfo); 
-				} 
-				// Generará un atributo?
-				if (preg_match(DOC_COMMENT_RENDER_ATTRIBUTE_PATTERN, $methodDocumentation, $matches))
-				{
-					$methodInfo->render_attribute = true;
-					$methodInfo->type_of_render = 'A';
-					$methodInfo->tag = new \stdClass();
-					$methodInfo->tag->render_as = array();
-					$this->BuildRenderDecorator($matches[1], $methodInfo);
-					//echo "<pre>";print_r ($methodInfo); 
-				} 
-				// Solo uno a la vez
-				if ( ($methodInfo->render_tag + $methodInfo->render_class + $methodInfo->render_attribute) > 1)
-					throw new \Exception("Solo puede haber una declaración de renderTar, renderAttribute o renderClass", 1);
-				
-				// No pueden coexistir bindVariable y bindModel!
-				if (isset($methodInfo->bind_variable) && isset($methodInfo->bind_model))
-					throw new \Exception("El método '{$methodInfo->name}' no puede estar ligado a una variable y a un modelo al mismo tiempo.", -100);
-					
-				$methodInfo->parameters = array();
-				foreach ($method->GetParameters() as $parameter)
-					$methodInfo->parameters[] = $parameter->name;
-
-				// Por ahora (creo que para siempre!) hay una restricción que los métodos con parámetros no se pueden
-				// ejecutar al principio, porque se necesitaría agregar algún valor de inicialización... y eso es demasiado 
-				// complicado de implementar considerando todas las posibles variantes
-				if ($methodInfo->run_on_load && count($methodInfo->parameters) > 0)
-					throw new \Exception("El método '{$methodInfo->name}' no puede ser ejecutado al inicio porque tiene parámetros.", -100);
-					
-								
-				$result[$method->getName()] = $methodInfo;
-			}
+        //echo "<pre>"; print_r ($this->GetController ()); die();
+        $controller = $this->GetController ();
+        if (isset($controller)) 
+        {
+    		$class = new \ReflectionClass ($controller);
+    		$methods = $class->getMethods(\ReflectionMethod::IS_PUBLIC);
+    		foreach ($methods as $method)
+    		{
+    			$methodDocumentation = $method->getDocComment();
+    			// Se procesa si el método tiene el decorator
+    			if (preg_match(DOC_COMMENT_AJAX_METHOD_PATTERN, $methodDocumentation))
+    			{
+    				$methodInfo = new \stdClass();
+    				$methodInfo->name = $method->getName();
+    				$methodInfo->is_global = $this->IsGlobal(); // Para garantizar que el servicio se genere o local o usando el global.
+    				$methodInfo->run_on_load = preg_match(DOC_COMMENT_RUN_ON_LOAD_PATTERN, $methodDocumentation);
+    				// Tiene variables asociadas?
+    				if (preg_match(DOC_COMMENT_BIND_VARIABLE_PATTERN, $methodDocumentation, $matches))
+    					$methodInfo->bind_variable =$matches[1]; // Se toma el nombre de la variable que genera este valor
+    				//Está conectado al modelo de datos?
+    				if (preg_match(DOC_COMMENT_BIND_DATA_MODEL_PATTERN, $methodDocumentation, $matches))
+    					$methodInfo->bind_model =$matches[1]; // Se toma el nombre de la variable que genera este valor
+    
+    				//Qué tipo de salida tiene?
+    				if (preg_match(DOC_COMMENT_CONTENT_TYPE_PATTERN, $methodDocumentation, $matches))
+    					$methodInfo->contentType =$matches[1]; // Se toma el nombre de la variable que genera este valor
+    
+    				// Generará un tag?
+    				if (preg_match(DOC_COMMENT_RENDER_TAG_PATTERN, $methodDocumentation, $matches))
+    				{
+    					$methodInfo->render_tag = true;
+    					$methodInfo->type_of_render = 'E';
+    					$methodInfo->tag = new \stdClass();
+    					$methodInfo->tag->render_as = array();
+    					$this->BuildRenderDecorator($matches[1], $methodInfo);
+    					//echo "<pre>";print_r ($methodInfo);
+    				}
+    				// Generará una clase?
+    				if (preg_match(DOC_COMMENT_RENDER_CLASS_PATTERN, $methodDocumentation, $matches))
+    				{
+    					$methodInfo->render_class = true;
+    					$methodInfo->type_of_render = 'C';
+    					$methodInfo->tag = new \stdClass();
+    					$methodInfo->tag->render_as = array();
+    					$this->BuildRenderDecorator($matches[1], $methodInfo);
+    					//echo "<pre>";print_r ($methodInfo);
+    				}
+    				// Generará un atributo?
+    				if (preg_match(DOC_COMMENT_RENDER_ATTRIBUTE_PATTERN, $methodDocumentation, $matches))
+    				{
+    					$methodInfo->render_attribute = true;
+    					$methodInfo->type_of_render = 'A';
+    					$methodInfo->tag = new \stdClass();
+    					$methodInfo->tag->render_as = array();
+    					$this->BuildRenderDecorator($matches[1], $methodInfo);
+    					//echo "<pre>";print_r ($methodInfo);
+    				}
+    				// Solo uno a la vez
+    				if ( ($methodInfo->render_tag + $methodInfo->render_class + $methodInfo->render_attribute) > 1)
+    					throw new \Exception("Solo puede haber una declaración de renderTar, renderAttribute o renderClass", 1);
+    
+    				// No pueden coexistir bindVariable y bindModel!
+    				if (isset($methodInfo->bind_variable) && isset($methodInfo->bind_model))
+    					throw new \Exception("El método '{$methodInfo->name}' no puede estar ligado a una variable y a un modelo al mismo tiempo.", -100);
+    
+    				$methodInfo->parameters = array();
+    				foreach ($method->GetParameters() as $parameter)
+    					$methodInfo->parameters[] = $parameter->name;
+    
+    				// Por ahora (creo que para siempre!) hay una restricción que los métodos con parámetros no se pueden
+    				// ejecutar al principio, porque se necesitaría agregar algún valor de inicialización... y eso es demasiado
+    				// complicado de implementar considerando todas las posibles variantes
+    				if ($methodInfo->run_on_load && count($methodInfo->parameters) > 0)
+    					throw new \Exception("El método '{$methodInfo->name}' no puede ser ejecutado al inicio porque tiene parámetros.", -100);
+    
+    
+    				$result[$method->getName()] = $methodInfo;
+    			}
+    		}
 		}
-		
+
 		return $result;
 	}
 
@@ -323,7 +373,7 @@ class SkyDataService extends SkyDataResource implements IService
 						if (!empty($methodInfo->bind_model))
 							throw new \Exception('El método no puede estar asociado a un modelo de datos si ha sido marcado con "fullHtml" ', -100);
 						// Ya se puede confirmar que genera HTML
-						$methodInfo->tag->is_html = $value == 'true' ; 
+						$methodInfo->tag->is_html = $value == 'true' ;
 						break;
 					case 'name'			: $methodInfo->tag->name = $value; break;
 					case 'templateurl'	: $methodInfo->tag->template_url = trim ($value,"'"); break;
@@ -345,7 +395,7 @@ class SkyDataService extends SkyDataResource implements IService
 									throw new \Exception("El tiempo indicado para el refresh no es válido", -100);
 							}
 							$methodInfo->refresh = $count;
-						}						 
+						}
 						break;
 					case 'showloading': $methodInfo->showLoading = (empty($value) || $value == 'true'); break;
 					case 'prerenderview' : $methodInfo->preRenderView = trim ($value,"'"); break;
@@ -353,15 +403,20 @@ class SkyDataService extends SkyDataResource implements IService
 				}
                 if (empty($methodInfo->tag->trigger))
                     $methodInfo->tag->trigger = 'init'; // Por defecto es "init"
-                
+
 				if  (!isset($methodInfo->tag->name))
 					throw new \Exception("El servicio debe dar un nombre al atributo, elemento o clase (use name en el métod {$methodInfo->name})", 1);
-				
+
 			}
 		else
 			throw new \Exception("Error de sintaxis definiendo 'renderTag'.", -100);
-        
+
         //echo "<pre>";print_r ($methodInfo); die();
 	}
-	
-} 
+
+    public function AddScript ($scriptPath)
+    {
+        $this->GetMetadataManager()->AddScript ($scriptPath);        
+    }
+
+}
